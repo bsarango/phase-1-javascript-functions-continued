c8e2a787fdee015ba99c926afd672b43ccbdb5fd
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-04-10 22:49:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+function saturdayFun(funEvent){
+    if(typeof funEvent === "string"){
+        return `This Saturday, I want to ${funEvent}!`;
+    }
+    else{
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+
+const mondayWork = function (monEvent){
+    if(typeof monEvent === "undefined"){
+        return "This Monday, I will go to the office.";
+    }
+    else{
+        return `This Monday, I will ${monEvent}.`;
+    }
+}
+
+
+function wrapAdjective(wrapSymbol){
+    
+    return function(yourPhrase){
+        if(typeof yourPhrase === "string" && typeof wrapSymbol === "string"){
+            return `You are ${wrapSymbol}${yourPhrase}${wrapSymbol}!`;
+        }
+        else if(typeof yourPhrase === "undefined" && typeof wrapSymbol === "string"){
+            return `You are ${wrapSymbol}special${wrapSymbol}!`;
+        }
+        else if(typeof yourPhrase === "string" && typeof wrapSymbol === "undefined"){
+            return `You are *${yourPhrase}*!`;
+        }
+        else{
+            return "you are *special*!";
+        }
+    }
+}
+    
+       
